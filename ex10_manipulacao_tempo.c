933d66f602d7372197bd99a9718fa9f861035fd2
Linejesus <161092862+Linejesus@users.noreply.github.com>
2024-05-19 19:56:49 +0000

    Exercício de calcular o tempo que o código demora para ser executado

--- ex10_manipulacao_tempo.c
+++ ex10_manipulacao_tempo.c
@@ -1,9 +1,20 @@
 #include <stdio.h> 
+#include <time.h>
 #include "funcoes.h"
 
 ERROS manipulacao_tempo()
 {
-  printf("Exercício 10: Manipulação de Tempo\n");
+  clock_t tempo_inicial = clock();
+
+  for (int i = 0; i < 1000000000; ++i)
+  {
+    printf("");
+  }
+
+  clock_t tempo_final = clock();
+  float r = tempo_final - tempo_inicial;
+
+  printf("\nTempo de duração do programa: %f s\n", r / CLOCKS_PER_SEC);
 
   return OK;
 }
