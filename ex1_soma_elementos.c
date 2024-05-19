2014ddab05b36018933396fc1f1b81123f8bf9ef
Linejesus <161092862+Linejesus@users.noreply.github.com>
2024-05-19 16:27:28 +0000

    Exercício de somar elementos em um array

--- ex1_soma_elementos.c
+++ ex1_soma_elementos.c
@@ -3,7 +3,28 @@
 
 ERROS soma_elementos()
 {
-  printf("Exercício 1: Soma de Elementos em um Array\n");
+  int i;
+  int quant;
+
+  printf("\nDigite a quantidade de números a serem somados: ");
+  scanf("%d", &quant);
+
+  int array[quant];
+
+  for(int j = 0; j < quant; j++)
+  {
+    printf("Digite um número: ");
+    scanf("%d", &i);
+    array[j] = i;
+  }
+
+  int soma = 0;
+  for (int j = 0; j < quant; j++)
+  {
+    soma += array[j];
+  }
+
+  printf("\nA soma dos números é: %d\n", soma);
   
   return OK;
 }
