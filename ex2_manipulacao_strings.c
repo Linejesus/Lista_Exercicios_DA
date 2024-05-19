954dd037fe2c11520a3a6fc164ec0b16c0234787
Linejesus <161092862+Linejesus@users.noreply.github.com>
2024-05-19 17:49:24 +0000

    Exercício de contar vogais em uma string

--- ex2_manipulacao_strings.c
+++ ex2_manipulacao_strings.c
@@ -1,9 +1,43 @@
-#include <stdio.h> 
 #include "funcoes.h"
+#include <stdio.h> 
+#include <string.h>
+
+void clearBuffer() { 
+  int c; 
+  while ((c = getchar()) != '\n' && c != EOF); 
+} 
+
+void upper(char s[])
+{
+  for(int i = 0; s[i] != '\0'; i++)
+  {
+    if(s[i] >= 'a' && s[i] <= 'z')
+    {
+      s[i] -= 32;
+    }
+  }
+}
 
 ERROS manipulacao_strings()
 {
-  printf("Exercício 2: Manipulação de Strings\n");
+  int vogal = 0;
+  char string[100];
+  
+  clearBuffer();
+  printf("\nDigite uma palavra: ");
+  fgets(string, 100, stdin);
+
+  upper(string);
+
+  for (int i = 0; string[i] != '\0'; i++)
+  {
+    if(string[i] == 'A' || string[i] == 'E' || string[i] == 'I' || string[i] == 'O' || string[i] == 'U')
+    {
+      vogal++;
+    }
+  }
+
+  printf("\nQuantidade de vogais: %d\n", vogal);
 
   return OK;
 }
